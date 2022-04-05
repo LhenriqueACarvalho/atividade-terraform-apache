2252fad6a7927afd4aa239cf26fc064604ddb825
Luiz Henrique Alves de Carvalho <lhenriqueacarvalho@gmail.com>
2022-04-04 21:40:05 -0300

    commit

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "location" {
+  description = "Localizacao do Servidor acessado no Azure"
+  default  = "australiaeast"
+}
+
+variable "user" {
+  description = "User da virtual machine"
+  default  = "luiz.acarvalho@aluno.faculdadeimpacta.com.br"
+}
+
+variable "password" {
+  description = "pwd User - apagar antes de subir"
+  default  = "Luiz2608"
+}
+
